562cd1e179b4f332688719e3035fec378c7efd49
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-09 14:20:57 +0100

    add

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Section = () => {
   return (
-    <div className='bg-pink-300 h-[50px] hidden md:block'>
+    <div className='bg-pink-300 h-[50px] hidden md:block tracking-widest'>
       <div className='flex justify-between items-center h-full px-4'>
         <button className='text-sm font-bold border-2  border-black px-2 py-1 rounded-none'>Women</button>
-        <p className='text-sm font-bold '>UP TO 50% OFF
+        <p className='text-sm font-bold text-center'>UP TO 50% OFF<br></br>CERTIFIED BANGERS!
         </p>
         <button className='text-sm font-bold border-2 border-black px-2 py-1 rounded-none'>Men</button>
       </div>
