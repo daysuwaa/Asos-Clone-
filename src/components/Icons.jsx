70fded2e03fd09a8bb993dc8c8d31c75adc80c8c
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-11 10:58:50 +0100

    Icons done

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -11,15 +11,15 @@ import American from '../assets/american.jpeg';
 const Icons = () => {
   return (
     <div className='h-[35px] hidden sm;hidden md:block lg:block'>
-      <div className='flex justify-center mt-[10px] rounded-full '>
-        <img src={Facebook} alt='Facebook' className='w-[30px] h-[30px] rounded-full mr-5' />
-        <img src={Instagram} alt='Instagram' className='w-[30px] h-[30px] rounded-full mr-5 ' />
-        <img src={Snapchat} alt='Snapchat' className='w-[30px] h-[30px] rounded-full mr-5' />
-        <div className='border-r border-black'></div>
-        <img src={Visa} alt='Visa' className='w-[40px] h-[30px] mr-6 ml-5' />
-        <img src={Master} alt='Mastercard' className='w-[40px] h-[30px] mr-4 ' />
-        <img src={Paypal} alt='paypal' className='w-[40px] h-[30px] mr-4 ' />
-        <img src={American} alt='american express' className='w-[40px] h-[30px] mr-4 ' />
+      <div className='flex justify-center mt-[12px] rounded-full '>
+        <img src={Facebook} alt='Facebook' className='w-[30px] h-[30px] rounded-full mr-[3rem]' />
+        <img src={Instagram} alt='Instagram' className='w-[30px] h-[30px] rounded-full mr-[3rem] ' />
+        <img src={Snapchat} alt='Snapchat' className='w-[30px] h-[30px] rounded-full mr-[3rem]' />
+        <div className='border-r border-gray-400'></div>
+        <img src={Visa} alt='Visa' className='w-[45px] h-[30px] mr-9 ml-5' />
+        <img src={Master} alt='Mastercard' className='w-[47px] h-[30px] mr-9 ' />
+        <img src={Paypal} alt='paypal' className='w-[47px] h-[30px] mr-4 ' />
+        <img src={American} alt='american express' className='w-[47px] h-[30px] mr-4 ' />
       </div>
     </div>
    
